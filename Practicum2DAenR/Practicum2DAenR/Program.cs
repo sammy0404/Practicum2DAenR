f239a93d77e13e467928ce7985f30a3fd22a0f4b
Sammie <samvdwal@gmail.com>
2014-06-12 12:17:45 +0200

    Sam zijn deel voor NOS
    Hier heeft Gerben helemaal niets aan gedaan

--- Practicum2DAenR/Practicum2DAenR/Program.cs
+++ Practicum2DAenR/Practicum2DAenR/Program.cs
@@ -3,13 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using HtmlAgilityPack;
 
-namespace Practicum2DAenR
+namespace ConsoleApplication15
 {
     class Program
     {
+        static StreamWriter w = new StreamWriter("test.txt");
         static void Main(string[] args)
         {
+            Console.ReadLine();
+            WebClient webcle = new WebClient();
+            int counter = 0;
+            int start = 659150;
+            while (counter < 100)
+            {
+                try
+                {
+                    string s = webcle.DownloadString("http://nos.nl/artikel/" + start);
+                    addToList(s);
+                    counter++;
+                    start--;
+                }
+                catch (Exception e)
+                {
+                    start--;
+                }
+            }
+            w.Close();
+        }
+        static void addToList(string s)
+        {
+            HtmlDocument d = new HtmlDocument();
+            d.LoadHtml(s);
+            HtmlNode n = d.GetElementbyId("article");
+            List<HtmlNode> ns = n.SelectSingleNode("div").SelectNodes("p").ToList();
+            string tekst = "";
+            foreach (HtmlNode node in ns)
+            {
+                tekst += " " + node.InnerText;
+            }
+            w.WriteLine(tekst);
+
+
         }
     }
 }
